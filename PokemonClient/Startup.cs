776d6c3f7b1c2adbc730a81fdb35e432f965a24f
Chris Ramer <chrisramer.97@gmail.com>
2021-07-02 11:02:11 -0700

    Initial commit

---
+++ PokemonClient/Startup.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PokemonClient
+{
+	public class Startup
+	{
+		public Startup(IHostingEnvironment env)
+		{
+			IConfigurationBuilder builder = new ConfigurationBuilder()
+			  .SetBasePath(env.ContentRootPath)
+			  .AddJsonFile("appsettings.json");
+			Configuration = builder.Build();
+		}
+
+		public IConfigurationRoot Configuration { get; set; }
+
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc();
+		}
+
+		public void Configure(IApplicationBuilder app)
+		{
+			app.UseDeveloperExceptionPage();
+			app.UseStaticFiles();
+
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+			name: "default",
+			template: "{controller=Home}/{action=Index}/{id?}");
+			});
+
+			app.Run(async (context) =>
+			{
+				await context.Response.WriteAsync("Something went wrong!");
+			});
+		}
+	}
+}
