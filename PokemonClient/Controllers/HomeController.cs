c2130dfa697eeea33a00525711558d2862f7931f
Chris Ramer <chrisramer.97@gmail.com>
2021-07-08 09:56:56 -0700

    Make dropdown for version selection

--- PokemonClient/Controllers/HomeController.cs
+++ PokemonClient/Controllers/HomeController.cs
@@ -1,12 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
 
 namespace PokemonClient.Controllers
 {
 	public class HomeController : Controller
 	{
+		static List<string> apiVersions = new List<string>()
+		{
+			"1.0",
+			"2.0"
+		};
+
 		[HttpGet("/")]
 		public ActionResult Index()
 		{
+			List<SelectListItem> versions = apiVersions.ConvertAll(v => new SelectListItem
+			{
+				Text = v,
+				Value = v,
+			});
+			ViewBag.VersionList = versions;
 			return View();
 		}
 	}
